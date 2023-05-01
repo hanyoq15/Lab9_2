cb6448388b2710325e0dedbeebc4770bdb3daa5b
hanyoq15 <130302221+hanyoq15@users.noreply.github.com>
2023-05-01 20:05:15 +0900

    Delete manager.h

--- manager.h
+++
@@ -1,7 +0,0 @@
-#include "product.h"
-
-void listProduct(Product *p,int count);
-int selectMenu();
-int selectDataNo(Product *p, int count);
-void saveData(Product *p, int count);
-int loadData(Product *p);
