8f93a736bae291a82314dfb49513935b880701c0
김채린 <rlacofls@rlacofls-ui-MacBookAir.local>
2023-05-01 23:51:10 +0900

    update manager.h

--- manager.h
+++ manager.h
@@ -1,8 +1,8 @@
 #include "product.h"
 
 int selectMenu();
-int selectDataNo(Product *p, int count);
 void listProduct(Product *p,int count);
+int selectDataNo(Product *p, int count);
 
 void saveData(Product *p, int count);
 int loadData(Product *p);
